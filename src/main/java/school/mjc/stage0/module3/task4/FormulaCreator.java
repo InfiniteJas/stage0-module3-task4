0a4aee9eb53a1b2618e78048616f39fa0e0cbfe2
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-02 12:43:29 +0600

    Module 3 task 4 - 2

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,7 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        double a, b;
+        double a;
+        a = 1.0;
+        double b;
+        b = 2.0;
         double result = ((9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
         System.out.println(result);
     }
